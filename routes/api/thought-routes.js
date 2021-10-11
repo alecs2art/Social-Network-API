99d1584c43bbfae294022169674ccfdcc4746dfc
unknown <alecs2art@gmail.com>
2021-10-11 15:25:27 -0700

    routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { 
+    getAllThoughts, 
+    getThoughtById, 
+    addThought, 
+    updateThought,
+    removeThought,
+    } = require('../../controllers/thought-controller');
+
+router.route('/').get(getAllThoughts).post(addThought);
+router.route('/:id').get(getThoughtById).put(updateThought).delete(removeThought);
+
+module.exports = router;
